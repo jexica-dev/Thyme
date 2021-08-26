96779f90133f8e02b7d11f4b638941984bc523d0
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2021-08-25 22:05:53 -0400

    adding time-stamps

--- client/src/components/Thymeline.jsx
+++ client/src/components/Thymeline.jsx
@@ -16,13 +16,11 @@ function Thymeline(props) {
   return (
     <div className="thymeline-container">
       {props.leaves.map((leaf, index) => {
-        const { time } = leaf.createdTime;
         const { message } = leaf.fields;
-
         return (
           <article className="thymeline-article">
             <h3>{message}</h3>
-            <h5>{time}</h5>
+            <h5>{leaf.createdTime}</h5>
             <br />
             <Link
               onClick={() => {
